8ba435d943192afc6a9dd2fb848a0b311b8b26bd
mountainwen <12750432@gmail.com>
2024-10-17 16:39:31 +0800

    week06

---
+++ week06/week06-5.cpp
@@ -0,0 +1,19 @@
+class Solution {
+public:
+    int maximumSwap(int num) {
+        int ans = num;//最簡單的答案,就是本身
+        string s = to_string(num);//stoi(反過來)
+        //for (int i=0;i<s.length();i++){//中間要用迴圈.更新答案
+        //cout << s[i] << ' ';//練習印看看
+        //}
+        for (int i=0;i<s.length()-1;i++){//左手i
+            for (int j=i+1;j<s.length();j++)//右手j
+            {
+                swap(s[i],s[j]);//交換s[i] s[j] 兩數
+                ans=max(ans,stoi(s));//看是否更大,更新
+                swap(s[i],s[j]);//交換回來
+            }
+        }
+        return ans;
+    }
+};
