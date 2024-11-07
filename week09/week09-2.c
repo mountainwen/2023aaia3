9a8d8451b97d63c18eb41281998764e3ef280771
mountainwen <12750432@gmail.com>
2024-11-07 15:05:48 +0800

    week09

---
+++ week09/week09-2.c
@@ -0,0 +1,15 @@
+//richest customer wealth
+//最有錢的人，有多少錢
+int maximumWealth(int** accounts, int accountsSize, int* accountsColSize) {
+    int M =accountsSize;
+    int N =accountsColSize[0];
+    int ans =0;
+    for(int i=0;i<M;i++){
+        int total=0;
+        for(int j=0;j<N;j++){
+            total+=accounts[i][j];
+        }
+        if(total>ans) ans=total;
+    }
+    return ans;
+}
