0b7df86e7a8a38ff2a071274c1e065f867c91938
mountainwen <12750432@gmail.com>
2024-11-07 15:24:14 +0800

    week09

--- week09/week09-2.cpp
+++ week09/week09-2.cpp
@@ -1,6 +1,8 @@
 //matrix diagonal sum
-int diagonalSum(int** mat, int matSize, int* matColSize) {
-    int M=matSize;
+class Solution {
+public:
+    int diagonalSum(vector<vector<int>>& mat) {
+        int M=mat.size();
         int ans=0;
         for(int i=0;i<M;i++){
             ans+=mat[i][i];//左上,右下
@@ -9,5 +11,5 @@ int diagonalSum(int** mat, int matSize, int* matColSize) {
         //單數時,要扣掉中間
         if(M%2==1) ans-=mat[M/2][M/2];
         return ans;
-
-}
+    }
+};
