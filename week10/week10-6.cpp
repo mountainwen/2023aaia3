dfb821e9eb8718c0e3c72d840bd9430ecaadef15
mountainwen <12750432@gmail.com>
2024-11-14 16:29:44 +0800

    week10

---
+++ week10/week10-6.cpp
@@ -0,0 +1,14 @@
+// largest perimeter of a triangle
+class Solution {
+public:
+    int largestPerimeter(vector<int>& nums) {
+        sort(nums.begin(),nums.end());
+        int N=nums.size();
+        for(int i=0;i<N-2;i++){
+            if(nums[N-1-i]<nums[N-2-i]+nums[N-3-i]){ 
+                return nums[N-1-i]+nums[N-2-i]+nums[N-3-i];
+            }
+        }
+        return 0;
+    }
+};
