417e3cc8145c524caf625587edd3460b83e0e76f
mountainwen <12750432@gmail.com>
2024-11-21 16:19:56 +0800

    week11

---
+++ week11/week11-5.cpp
@@ -0,0 +1,16 @@
+//sign of the product of an array
+class Solution {
+public:
+    int arraySign(vector<int>& nums) {
+        int ans=1;
+        for(int num:nums){
+            if(num>0) ans*=1;
+            if(num==0) ans*=0;
+            if(num<0) ans*=-1;
+            
+        }
+        if(ans>0) return 1;
+        else if(ans==0) return 0;
+        else return -1;
+    }//這個寫法是錯的
+};
