060c18dd958fd1548deb54151b086f10ff35b6c1
mountainwen <12750432@gmail.com>
2024-12-05 14:34:23 +0800

    week13

--- week13/week13-1.cpp
+++ week13/week13-1.cpp
@@ -1,4 +1,5 @@
 //primary arithmetic(未完成)
+#include <iostream>
 using namespace std;
 int helper(int a,int b)//step03:function
 {
@@ -11,9 +12,9 @@ int main()
 	{
 		if(a==0 && b==0) break;
 		int ans=helper(a,b);//step03:function
-		if(ans==0) cout<<"No carry operation. \n";
-		else if(ans==1) cout<<"1 carry operation. \n";
-		else cout<<ans<<" carry operations. \n";
+		if(ans==0) cout<<"No carry operation.\n";
+		else if(ans==1) cout<<"1 carry operation.\n";
+		else cout<<ans<<" carry operations.\n";
 		
 	}//step02:output
 }
