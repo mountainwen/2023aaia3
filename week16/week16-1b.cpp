2300de1165e94053181409ebb5226b0ec6ff0c6b
mountainwen <12750432@gmail.com>
2024-12-26 14:27:59 +0800

    week16

---
+++ week16/week16-1b.cpp
@@ -0,0 +1,15 @@
+//A的B次方
+#include <stdio.h>
+#include <math.h>
+int	MYPOWER(int a,int b)
+{
+
+	return pow(a,b);
+}
+int main(void)
+{
+	int a,b;
+	scanf("%d%d",&a,&b);
+	printf("[%d]",MYPOWER(a,b));
+	return 0;
+}
